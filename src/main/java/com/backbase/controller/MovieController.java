ac01bea5ae614e76f995ceeff5b083633e55d3ec
Sabdavi <abdavi.saeed@gmail.com>
2025-04-30 12:11:49 +0200

    add basic repository and service

---
+++ src/main/java/com/backbase/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.backbase.controller;
+
+import com.backbase.service.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public boolean wonBestPicture(@RequestParam() String movieTitle) {
+        return  movieService.wonBestPicture(movieTitle);
+
+    }
+}
